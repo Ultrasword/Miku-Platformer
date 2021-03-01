709251d90aeb4c41e154ca55e4e1b7e2f26b5161
mynameispet <34387360+Ultrasword@users.noreply.github.com>
2021-03-01 08:37:47 -0500

    Game files

---
+++ game/pack/Happy Texture.tsx
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Happy Texture" tilewidth="32" tileheight="32" tilecount="26" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="dirt.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="dirt_bottom.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="dirt_curve_left.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="dirt_curve_right.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="dirt_left.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="dirt_right.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="grass.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="grass_curve_left.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="grass_curve_right.png"/>
+ </tile>
+ <tile id="10" type="Coin">
+  <properties>
+   <property name="type" value="Coin"/>
+  </properties>
+  <image width="32" height="32" source="coin.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="type" value="None"/>
+  </properties>
+  <image width="32" height="32" source="beneathfloor.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="floor.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="floor_bottom.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="floor_bottom_left.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="floor_bottom_right.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="floor_left.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="floor_middle_left.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="floor_middle_right.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="floor_right.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="one_block_floor.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="one_block_floor_left.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="one_block_floor_middle.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="one_block_floor_right.png"/>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="type" value="doorbottom"/>
+  </properties>
+  <image width="32" height="32" source="doorbottom.png"/>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="type" value="door"/>
+  </properties>
+  <image width="32" height="32" source="doortop.png"/>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="type" value="jboost"/>
+  </properties>
+  <image width="32" height="32" source="jump_boost.png"/>
+ </tile>
+</tileset>
---
+++ game/pack/Piano Boss.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Piano Boss" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="Key_Bottom"/>
+  </properties>
+  <image width="32" height="32" source="piano_bottom.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="Key"/>
+  </properties>
+  <image width="32" height="32" source="piano_top.png"/>
+ </tile>
+</tileset>
